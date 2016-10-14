6776d9242e520acee969f3bfb8bffb6a794be2c5
Martin Schröder <mkschreder.uk@gmail.com>
2016-10-14 15:27:55 +0200

    update default config

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -1,115 +1,94 @@
-/*
-	FreeRTOS.org V5.4.2 - Copyright (C) 2003-2009 Richard Barry.
-
-	This file is part of the FreeRTOS.org distribution.
-
-	FreeRTOS.org is free software; you can redistribute it and/or modify it
-	under the terms of the GNU General Public License (version 2) as published
-	by the Free Software Foundation and modified by the FreeRTOS exception.
-
-	FreeRTOS.org is distributed in the hope that it will be useful,	but WITHOUT
-	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-	FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-	more details.
-
-	You should have received a copy of the GNU General Public License along
-	with FreeRTOS.org; if not, write to the Free Software Foundation, Inc., 59
-	Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-
-	A special exception to the GPL is included to allow you to distribute a
-	combined work that includes FreeRTOS.org without being obliged to provide
-	the source code for any proprietary components.  See the licensing section
-	of http://www.FreeRTOS.org for full details.
-
-
-	***************************************************************************
-	*                                                                         *
-	* Get the FreeRTOS eBook!  See http://www.FreeRTOS.org/Documentation      *
-	*                                                                         *
-	* This is a concise, step by step, 'hands on' guide that describes both   *
-	* general multitasking concepts and FreeRTOS specifics. It presents and   *
-	* explains numerous examples that are written using the FreeRTOS API.     *
-	* Full source code for all the examples is provided in an accompanying    *
-	* .zip file.                                                              *
-	*                                                                         *
-	***************************************************************************
-
-	1 tab == 4 spaces!
-
-	Please ensure to read the configuration and relevant port sections of the
-	online documentation.
-
-	http://www.FreeRTOS.org - Documentation, latest information, license and
-	contact details.
-
-	http://www.SafeRTOS.com - A version that is certified for use in safety
-	critical systems.
-
-	http://www.OpenRTOS.com - Commercial support, development, porting,
-	licensing and training services.
-*/
-
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
 /*-----------------------------------------------------------
- * Application specific definitions.
- *
- * These definitions should be adjusted for your particular hardware and
- * application requirements.
- *
- * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
- *
- * See http://www.freertos.org/a00110.html.
- *----------------------------------------------------------*/
+* Application specific definitions.
+*
+* These definitions should be adjusted for your particular hardware and
+* application requirements.
+*
+* THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
+* FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
+*
+* See http://www.freertos.org/a00110.html.
+*----------------------------------------------------------*/
 
-#define configUSE_TIMERS				0
-#define configTIMER_TASK_PRIORITY 		0
-#define configTIMER_QUEUE_LENGTH 		1
-#define configTIMER_TASK_STACK_DEPTH 	0
-#define configUSE_MALLOC_FAILED_HOOK 	1
+/**
+ * @addtogroup PIOS PIOS
+ * @{
+ * @addtogroup FreeRTOS FreeRTOS
+ * @{
+ */
 
-#define configCPU_CLOCK_HZ				CPU_CLOCK_FREQ
-#define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				1
-#define configUSE_TICK_HOOK				1
-#define configTICK_RATE_HZ				( ( portTickType ) 1000 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 80 ) /* This can be made smaller if required. */
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 1 * 1024 ) )
-#define configMAX_TASK_NAME_LEN			( 16 )
-#define configUSE_TRACE_FACILITY    	0
-#define configIDLE_SHOULD_YIELD			1
-#define configUSE_CO_ROUTINES 			0
-#define configUSE_16_BIT_TICKS			0
-#define configUSE_MUTEXES				1
-#define configUSE_COUNTING_SEMAPHORES	1
-#define configUSE_ALTERNATIVE_API		0
-#define configUSE_RECURSIVE_MUTEXES		1
-#define configCHECK_FOR_STACK_OVERFLOW	0 /* Do not use this option on the PC port. */
-#define configUSE_APPLICATION_TASK_TAG	1
-#define configQUEUE_REGISTRY_SIZE		0
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY	1
-#define configSUPPORT_STATIC_ALLOCATION 1
+/* Notes: We use 5 task priorities */
 
-#define configMAX_PRIORITIES			( 4 )
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configCPU_CLOCK_HZ                           (CPU_CLOCK_FREQ)   // really the NVIC clock ...
+#define configTICK_RATE_HZ                           ((portTickType)1000)
+#define configMAX_PRIORITIES                         (5UL)
+#define configMINIMAL_STACK_SIZE                     ((unsigned short)128)
+#define configTOTAL_HEAP_SIZE                        ((size_t)(8 * 1024))  // this is minimum, not total
+#define configMAX_TASK_NAME_LEN                      (16)
+
+#define configUSE_PREEMPTION                         1
+#define configUSE_IDLE_HOOK                          0
+#define configUSE_TICK_HOOK                          0
+#define configUSE_TRACE_FACILITY                     0
+#define configUSE_16_BIT_TICKS                       0
+#define configIDLE_SHOULD_YIELD                      0
+#define configUSE_MUTEXES                            1
+#define configUSE_RECURSIVE_MUTEXES                  1
+#define configUSE_COUNTING_SEMAPHORES                0
+#define configUSE_ALTERNATIVE_API                    0
+#define configCHECK_FOR_STACK_OVERFLOW               0
+#define configQUEUE_REGISTRY_SIZE                    10
+
+#define configUSE_TIMERS                             0
+#define configTIMER_TASK_PRIORITY                    (configMAX_PRIORITIES - 1)      /* run timers at max priority */
+#define configTIMER_QUEUE_LENGTH                     10
+#define configTIMER_TASK_STACK_DEPTH                 configMINIMAL_STACK_SIZE
+
+/* Co-routine definitions. */
+#define configUSE_CO_ROUTINES                        0
+#define configMAX_CO_ROUTINE_PRIORITIES              (2)
 
 /* Set the following definitions to 1 to include the API function, or zero
-to exclude the API function. */
+   to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet        	1
-#define INCLUDE_uxTaskPriorityGet       	1
-#define INCLUDE_vTaskDelete             	1
-#define INCLUDE_vTaskCleanUpResources   	1
-#define INCLUDE_vTaskSuspend            	1
-#define INCLUDE_vTaskDelayUntil				1
-#define INCLUDE_vTaskDelay					1
-#define INCLUDE_uxTaskGetStackHighWaterMark 0 /* Do not use this option on the PC port. */
-#define INCLUDE_xTaskGetSchedulerState		1
+#define INCLUDE_vTaskPrioritySet                     1
+#define INCLUDE_uxTaskPriorityGet                    1
+#define INCLUDE_vTaskDelete                          1
+#define INCLUDE_vTaskCleanUpResources                1
+#define INCLUDE_vTaskSuspend                         1
+#define INCLUDE_vTaskDelayUntil                      1
+#define INCLUDE_vTaskDelay                           1
+#define INCLUDE_xTaskGetSchedulerState               1
+#define INCLUDE_xTaskGetCurrentTaskHandle            1
+#define INCLUDE_uxTaskGetStackHighWaterMark          1
+#define INCLUDE_xTaskGetIdleTaskHandle               1
 
-//#define traceQUEUE_SEND( pxQueue ) vMainQueueSendPassed()
+/* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
+   (lowest) to 1 (highest maskable) to 0 (highest non-maskable). */
+#define configKERNEL_INTERRUPT_PRIORITY             255 /* equivalent to NVIC priority 15 */
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY		191 /* equivalent to NVIC priority  3 */
 
-#define configGENERATE_RUN_TIME_STATS		0
+/* This is the value being used as per the ST library which permits 16
+   priority values, 0 to 15.  This must correspond to the
+   configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
+   NVIC value of 255. */
+#define configLIBRARY_KERNEL_INTERRUPT_PRIORITY      15
+
+/* Enable run time stats collection */
+#define configGENERATE_RUN_TIME_STATS                0
+#define INCLUDE_uxTaskGetRunTime                     0
+
+/* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
+standard names. */
+#define vPortSVCHandler SVC_Handler
+#define xPortPendSVHandler PendSV_Handler
+#define xPortSysTickHandler SysTick_Handler
+
+/**
+ * @}
+ */
 
 #endif /* FREERTOS_CONFIG_H */
